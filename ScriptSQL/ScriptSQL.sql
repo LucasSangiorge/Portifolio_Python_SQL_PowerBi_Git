06291ea4d04fc3e1713e73c42d20308c9b560196
Lucas Sangiorge <lucas.sangiorge@gmail.com>
2025-10-07 16:47:50 -0300

    Atualização de Scripts

--- ScriptSQL/ScriptSQL.sql
+++ ScriptSQL/ScriptSQL.sql
@@ -1,13 +1,25 @@
 SELECT 
     m.id_maquina,
-    m.nome_maquina,
+    m.tipo AS tipo_maquina,
+    m.status,
+	STRING_AGG(DISTINCT ma.responsavel, ', ') AS responsaveis_manutencao,
+	ma.tipo AS tipo_manutencao,
+	COALESCE(i.gravidade, 'Sem ocorrências') AS nivel_gravidade,
+	COUNT(i.id_incidente) AS total_incidentes,
+    m.data_aquisicao,
+    COALESCE(SUM(ma.custo), 0) AS custo_total_manutencao
+	
+FROM       maquinas    m
+LEFT JOIN  manutencoes ma  ON m.id_maquina = ma.id_maquina
+LEFT JOIN  incidentes  i   ON m.id_maquina = i.id_maquina
+GROUP BY 
+    m.id_maquina,
     m.tipo,
     m.status,
-    COALESCE(SUM(ma.custo),0) AS custo_total_manutencao,
-    COALESCE(SUM(EXTRACT(EPOCH FROM (ma.data_fim - ma.data_inicio))/3600),0) AS tempo_inatividade_horas,
-    COUNT(i.id_incidente) AS total_incidentes
-FROM maquinas m
-LEFT JOIN manutencoes ma ON m.id_maquina = ma.id_maquina
-LEFT JOIN incidentes i ON m.id_maquina = i.id_maquina
-GROUP BY m.id_maquina, m.nome_maquina, m.tipo, m.status
-ORDER BY m.id_maquina;
+    m.data_aquisicao,
+    ma.tipo,
+    i.gravidade
+ORDER BY 
+m.id_maquina;
+
+
